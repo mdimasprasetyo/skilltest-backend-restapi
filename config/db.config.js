7d1a6669dbe7496bd30af64e19b7a3fc3b1f56f9
Muhammad Dimas Prasetyo <mdimasp13@gmail.com>
2020-12-18 08:36:45 +0700

    using cleardb mysql

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "mdp",
-    PASSWORD: "bukandimas",
-    DB: "skilltest",
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b22c45369769e3",
+    PASSWORD: "56763e37",
+    DB: "heroku_7fa8e2d0290e9fb",
     dialect: "mysql",
     pool: {
         max: 5,
